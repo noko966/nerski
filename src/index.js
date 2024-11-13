e85b2e69f7decaebffdb1e54b24e2a77afc87269
nik <nikolay.kananov@digitain.com>
2024-11-13 20:45:39 +0400

    ic

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import init from './main.js';
+
+if (!window.init) {
+    window.init = init;
+} else {
+    console.warn('window.init already exists!');
+}
+
+(function(global) {
+    global.myApp = global.myApp || {};
+    global.myApp.init = init;
+})(window);
+
+// Add event listener for keydown event to trigger init on pressing "s"
+window.addEventListener('keydown', (event) => {
+    if (event.key === 's' || event.key === 'S') {  // Check for "s" or "S" key
+        init();  // Trigger the init function
+    }
+});
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Your entry point
+  output: {
+    filename: 'bundle.js', // Output filename
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      client: {
+        logging: 'none',
+        overlay: false, // Disable the error overlay
+      },
+    compress: true,
+    port: 8888,
+  },
+  mode: 'production', // or 'development'
+};
